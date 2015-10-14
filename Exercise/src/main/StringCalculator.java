a7a7404616299ea1250e85229e465e28cf9a84cf
vdkhanh <dk.vdkhanh@gmail.com>
2015-10-15 00:00:09 +0700

    [Refactor] Extract some special value to CONSTANT

--- Exercise/src/main/StringCalculator.java
+++ Exercise/src/main/StringCalculator.java
@@ -2,12 +2,19 @@ package main;
 
 public class StringCalculator {
 
+	private static final String SPLITTER = ",";
+	private static final int MAX_NUMBER_OF_INPUT = 2;
+	private static final int ZERO = 0;
+
 	public static int add(String numbers) {
 		if (numbers.isEmpty()) {
-			return 0;
+			return ZERO;
 		}
-		String[] numbersArray = numbers.split(",");
-		int sum = 0;
+		String[] numbersArray = numbers.split(SPLITTER);
+		if(numbersArray.length > MAX_NUMBER_OF_INPUT){
+			throw new RuntimeException();
+		}
+		int sum = ZERO;
 		for (String number : numbersArray) {
 			sum += Integer.parseInt(number); // If it is not a number, parseInt
 												// will throw an exception
