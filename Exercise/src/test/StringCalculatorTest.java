4c30e210a036510190d01965fac4af85f3d66a2d
vdkhanh <dk.vdkhanh@gmail.com>
2015-10-16 00:17:53 +0700

    [Refactor] refactor unit test

--- Exercise/src/test/StringCalculatorTest.java
+++ Exercise/src/test/StringCalculatorTest.java
@@ -8,32 +8,34 @@ import org.junit.Test;
 public class StringCalculatorTest {
 	@Test
 	public void whenInputEmptyThenReturnZero() {
-		Assert.assertTrue(StringCalculator.add("") == 0);
+		Assert.assertTrue(0 == StringCalculator.add(""));
 	}
 
 	@Test
 	public void whenInputOneNumberThenReturnThisNumber() {
-		Assert.assertTrue(StringCalculator.add("1") == 1);
-		Assert.assertTrue(StringCalculator.add("2") == 2);
+		Assert.assertTrue(1 == StringCalculator.add("1"));
+		Assert.assertTrue(2 == StringCalculator.add("2"));
 	}
 
 	@Test
 	public void whenInputTwoNumberThenReturnSumOfThem() {
-		Assert.assertTrue(StringCalculator.add("1,2") == 3);
-		Assert.assertTrue(StringCalculator.add("2,2") == 4);
+		Assert.assertTrue(1 + 2 == StringCalculator.add("1,2"));
+		Assert.assertTrue(2 + 2 == StringCalculator.add("2,2"));
 		Assert.assertTrue(StringCalculator.add("0,0") == 0);
 	}
-	
+
 	@Test
 	public void whenAnyNumberOfNumbersUsedThenReturnSumOfAll() {
-		Assert.assertTrue(1+2+3+4+5+6 == StringCalculator.add("1,2,3,4,5,6"));
+		Assert.assertTrue(1 + 2 + 3 + 4 + 5 + 6 == StringCalculator.add("1,2,3,4,5,6"));
 	}
+
 	@Test
 	public void whenInputTwoNumberWithNewLineThenReturnSumOfThem() {
-		Assert.assertTrue(StringCalculator.add("1\n2") == 3);
+		Assert.assertTrue(3 == StringCalculator.add("1\n2"));
 	}
+
 	@Test
 	public void whenInputMoreNumberWithNewLineAndCommaThenReturnSumOfThem() {
-		Assert.assertTrue(1+2+3 == StringCalculator.add("1\n2,3"));
+		Assert.assertTrue(1 + 2 + 3 == StringCalculator.add("1\n2,3"));
 	}
 }
