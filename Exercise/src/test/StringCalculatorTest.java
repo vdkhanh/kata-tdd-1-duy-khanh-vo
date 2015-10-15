704c9a611a66952e24bc0b5c65ea697f20c57d81
vdkhanh <dk.vdkhanh@gmail.com>
2015-10-16 00:23:39 +0700

    [AddTest] add test for new feature to support different delimiters then still return sum of all number

--- Exercise/src/test/StringCalculatorTest.java
+++ Exercise/src/test/StringCalculatorTest.java
@@ -38,4 +38,9 @@ public class StringCalculatorTest {
 	public void whenInputMoreNumberWithNewLineAndCommaThenReturnSumOfThem() {
 		Assert.assertTrue(1 + 2 + 3 == StringCalculator.add("1\n2,3"));
 	}
+	
+	@Test
+	public final void whenDelimiterIsSpecifiedThenUseItToSplitNumbers() {
+	    Assert.assertEquals(1+2, StringCalculator.add("//;\n1;2"));
+	}
 }
